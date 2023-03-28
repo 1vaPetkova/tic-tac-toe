02dac9813cd08ce5487853e4f0569f9722f3570e
Iva Petkova <iva1petkova@gmail.com>
2023-03-28 23:31:30 +0300

    add extra

--- src/App.js
+++ src/App.js
@@ -12,19 +12,20 @@ const winningCombinations = [
   [2, 4, 6]
 ];
 
-
-
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [turnHistory, setTurnHistory] = useState([null]);
   const [currentMove, setCurrentMove] = useState(0);
   const [sortAsc, setSortOrder] = useState(true);
+
   const xTurn = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
-  function handlePlay(nextSquares) {
+  function handlePlay(nextSquares, index) {
     const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHistory);
     setCurrentMove(nextHistory.length - 1);
+    setTurnHistory([...turnHistory.slice(0, currentMove + 1), index]);
   }
 
   function jumpTo(nextMove) {
@@ -36,11 +37,15 @@ export default function Game() {
   }
 
   const moves = history.map((squares, move) => {
-    let description = move === 0 ? 'Go to the game start' : `Go to move ${move}`;
+    let position = turnHistory[move];
+    let row = Math.floor(position/3);
+    let col = position % 3;
+    let description = move === 0 ? 'Go to the game start' : `Go to move ${move} (${row}, ${col})`;
     if (move === currentMove) {
+      let position = move === 0 ? "" : `(${row}, ${col})`;
       return (
       <li key={move}>
-        <div>You are at move {currentMove}</div>
+        <div>You are at move {currentMove} {position}</div>
       </li>
       );
     }
@@ -71,17 +76,23 @@ export default function Game() {
 
 
 function Board({ xTurn, squares, onPlay }) {
+
   function handleClick(index) {
+    console.log(index);
     if (checkForWinner(squares) || squares[index]) return;
     const nextSquares = squares.slice();
     nextSquares[index] = xTurn ? 'X' : "O";
-    onPlay(nextSquares);
+    onPlay(nextSquares, index);
   }
 
-  const winner = checkForWinner(squares);
+
+
+  const gameWinner = checkForWinner(squares);
+  const winnerValue = gameWinner ? gameWinner.winner : null;
   let status;
-  if (winner) {
-    status = `The ${winner} wins the game!`
+  if (winnerValue) {
+    status = `The ${winnerValue} wins the game!`
+   
   } else {
     if (!squares.filter(s => s === null).length) {
       status = 'Nobody wins!'
@@ -90,43 +101,52 @@ function Board({ xTurn, squares, onPlay }) {
     }
   }
 
-
   return (
     <div>
       <div className='status'>{status}</div>
       <div>
-      <Matrix squares={squares} handleClick={handleClick}/>  
+      <Matrix squares={squares} handleClick={handleClick} gameWinner={gameWinner}/>  
       </div>
     </div>
   )
 }
 
-var indices = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
+function Matrix ({squares, handleClick, gameWinner}) {
+  let indices = [[0, 1, 2], [3, 4, 5], [6, 7, 8]];
 
-function Matrix ({squares, handleClick}) {
+  let squareBackground = (col) => { if (gameWinner && gameWinner.winningCombination.includes(col)) return "green"}
+
   return (
-    indices.map(row =>
-      <div className='board-row'>
+    <div>
+    {indices.map(row =>
+      <div key={row} className='board-row'>
         {
           row.map(col => 
-            <Square value={squares[col]} onSquareClick={() => handleClick(col)} />
+            <Square key={col} value={squares[col]} onSquareClick={() => handleClick(col)} background={squareBackground(col)} />
           )
         }
       </div>
-    ));
+      
+    )}
+    </div>
+  )
 }
 
-
-function Square({ value, onSquareClick }) {
-  return <button className="square" onClick={onSquareClick}>{value}</button>;
+function Square({ value, onSquareClick, background }) {
+  return <button className="square" onClick={onSquareClick} style={{background}}>{value}</button>;
 }
 
 
 function checkForWinner(squares) {
   let result = null;
   winningCombinations.forEach(([a, b, c]) => {
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) result = squares[a];
-  });
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      result = {
+        winner: squares[a],
+        winningCombination: [a, b, c]
+      };
+    }
+});
   return result;
 }
 
